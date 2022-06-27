3d3548d1aa9afecc838eb2ead34042cbbe876c2d
ColonelBucket8 <freedomterran89@gmail.com>
2022-06-27 22:18:56 +0800

    added comment

--- app/Main.hs
+++ app/Main.hs
@@ -1,26 +1,44 @@
+-- initialize variable
 x :: Int
 x = 3
 
+-- function
 sumtorial :: Integer -> Integer
 sumtorial 0 = 0
 sumtorial x = x + sumtorial (x-1)
 
+-- function with multiple arguments
 f :: Int -> Int -> Int -> Int
 f x y z = x + y + z
 
+-- lists
 nums, range, range2 :: [Integer]
 nums = [1,2,3,4]
 range = [1..100]
 range2 = [2,4..100]
+
 
 hailstone :: Integer -> Integer
 hailstone n
     | n `mod` 2 == 0 = n `div` 2
     | otherwise      = 3*n + 1
 
+-- constructing lists
+ex21 = [2,3,4] == 2 : 3 : 4 : []
+
+-- function from integer to list
 hailstoneSeq :: Integer -> [Integer]
 hailstoneSeq 1 = [1]
 hailstoneSeq n = n : hailstoneSeq (hailstone n)
+
+-- pairs
+p :: (Int, Char)
+p = (3, 'x')
+
+-- compute the length of a list of Integers
+intListLength :: [Integer] -> Integer
+intListLength [] = 0
+intListLength (x:xs) = 1 + intListLength xs
 
 main :: IO ()
 main = do 
@@ -28,3 +46,5 @@ main = do
     print(f 1 2 5)
     print(hailstone 3)
     print(hailstoneSeq 3)
+    print(intListLength [1,1,1,1,1,1])
+
