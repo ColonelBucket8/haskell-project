019062c9eed43f711fe923f7773c5b9f4c522d67
ColonelBucket8 <freedomterran89@gmail.com>
2022-06-28 22:46:07 +0800

    added exercise 1

---
+++ src/Hw1Exercise1.hs
@@ -0,0 +1,8 @@
+module Hw1Exercise1 where
+
+-- convert digits to list
+-- zero and negative numbers return empty list
+toDigits :: Integer -> [Integer]
+toDigits 0 = []
+toDigits x | x < 0 = []
+toDigits x = toDigits (x `div` 10) ++ [x `mod` 10]
