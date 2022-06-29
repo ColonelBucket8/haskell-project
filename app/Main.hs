e83274e5018a0c51d401ae961e345fb6a010f61f
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2022-06-29 22:07:57 +0800

    added sum digits

--- app/Main.hs
+++ app/Main.hs
@@ -9,4 +9,6 @@ main = do
     print(toDigits(12348161))
     print(toDigitsRev(12345678))
     print(toDigitsRev(-1))
+    print(doubleEveryOther([1, 2, 3, 4, 5, 6, 7, 8, 9]))
+    print(sumDigits([1, 2, 3, 4, 5, 6, 11]))
 
--- src/Homework1.hs
+++ src/Homework1.hs
@@ -7,8 +7,17 @@ toDigits 0 = []
 toDigits x | x < 0 = []
 toDigits x = toDigits (x `div` 10) ++ [x `mod` 10]
 
--- conver digits to list reverse
+-- convert digits to list reverse
 toDigitsRev :: Integer -> [Integer]
 toDigitsRev 0 = []
 toDigitsRev x | x < 0 = []
 toDigitsRev x = x `mod` 10 : toDigitsRev(x `div` 10)
+
+-- double every 2nd element 
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther (x:y:xs) = x : y * 2 : doubleEveryOther xs
+doubleEveryOther a = a
+
+-- sum of all digits in a list 
+sumDigits :: [Integer] -> Integer
+sumDigits (x:xs)  = sum(x:xs)
