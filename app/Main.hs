3d205dfcd842983ab204d1fe860def71de0e6817
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2022-06-29 21:36:25 +0800

    added to digits rev

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,12 @@
 module Main where
 import IntroductionToHaskell
-import Homework2
+import Homework1
 
 main :: IO ()
 main = do 
     print(toDigits(0))
     print(toDigits(-1))
     print(toDigits(12348161))
+    print(toDigitsRev(12345678))
+    print(toDigitsRev(-1))
 
--- src/Homework1.hs
+++ src/Homework1.hs
@@ -6,3 +6,9 @@ toDigits :: Integer -> [Integer]
 toDigits 0 = []
 toDigits x | x < 0 = []
 toDigits x = toDigits (x `div` 10) ++ [x `mod` 10]
+
+-- conver digits to list reverse
+toDigitsRev :: Integer -> [Integer]
+toDigitsRev 0 = []
+toDigitsRev x | x < 0 = []
+toDigitsRev x = x `mod` 10 : toDigitsRev(x `div` 10)
