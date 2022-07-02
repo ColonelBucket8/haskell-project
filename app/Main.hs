f2ebd6185c509bb3d9893a0744991810944b1c7e
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2022-07-02 20:32:08 +0800

    added tower of hanoi

--- app/Main.hs
+++ app/Main.hs
@@ -4,12 +4,5 @@ import Homework1
 
 main :: IO ()
 main = do 
-    print(toDigits(0))
-    print(toDigits(-1))
-    print(toDigits(12348161))
-    print(toDigitsRev(12345678))
-    print(toDigitsRev(-1))
-    print(doubleEveryOther([1, 2, 3, 4, 5, 6, 7, 8, 9]))
-    print(sumDigits([1, 2, 3, 4, 5, 6, 11]))
-    print(validate 4012888888881881)
-
+    print(hanoi 5 "a" "c" "b")
+     
