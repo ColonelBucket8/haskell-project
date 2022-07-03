5f9c038432fad3947bf28194558dfc9846d8ea21
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2022-07-03 15:01:32 +0800

    added something

--- app/Main.hs
+++ app/Main.hs
@@ -7,10 +7,4 @@ import Homework.Homework2.LogAnalysis
 
 main :: IO ()
 main = do 
-    print(getAge brent)
-    print(baz brent)
-    print(checkFav brent) 
-    print(checkFav stan) 
-    print(intListProd Empty)
-    print(intListProd (Cons 4 Empty))
-
+    print("Hello World")
--- src/Homework/Homework2/LogAnalysis.hs
+++ src/Homework/Homework2/LogAnalysis.hs
@@ -1,4 +1,7 @@
 {-# OPTIONS_GHC -Wall #-}
 module Homework.Homework2.LogAnalysis where
 
-import Homework.Homework2.Log()
+import Homework.Homework2.Log as L
+
+parseMessage :: [ String ] -> L.LogMessage
+parseMessage ("E":error:timestamp:message) = L.LogMessage (L.Error (read error)) (read timestamp) ( unwords message )
