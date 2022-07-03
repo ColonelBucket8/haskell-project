52b62bd1ef2038f61cf3e7eb432323622ebb3f03
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2022-07-03 22:20:59 +0800

    added syntax in functions

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,10 @@
 module Main where
 import Lecture.IntroductionToHaskell
 import Lecture.AlgebraicDataTypes
+
 import LearnYouAHaskell.StartingOut as Starting
 import LearnYouAHaskell.TypesAndTypeclasses as Types
+import LearnYouAHaskell.SyntaxInFunctions as Syntax
 
 import Homework.Homework1
 import Homework.Homework2.LogAnalysis
@@ -11,9 +13,8 @@ main :: IO ()
 main = do 
     print("Hello World")
     print(drop 3 [1,2,3,4])
-    print(Starting.doubleFromTo 1 10)
-    print(Starting.numbersFromTo 1 20)
-    print(Starting.length' [1, 4, 5, 6, 7, 1])
-    print(Types.removeNonUppercase "fasDfaSdf")
     print(Starting.rightTriangles 3 4 5)
     print(Starting.rightTriangles 3 4 6)
+    print(Syntax.factorial 0)
+    print(Syntax.factorial 4)
+    print(Syntax.addVectors (1, 1) (2, 3))
---
+++ src/LearnYouAHaskell/SyntaxInFunctions.hs
@@ -0,0 +1,10 @@
+module LearnYouAHaskell.SyntaxInFunctions where
+
+-- Patern Matching
+factorial :: (Integral a) => a -> a
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
+
+addVectors :: (Num a) => (a, a) -> (a, a) -> (a, a)
+addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+
