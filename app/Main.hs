192a98a0e56318b1ce8510f075024dacd5d09aec
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2022-07-06 21:35:35 +0800

    added recursions

--- app/Main.hs
+++ app/Main.hs
@@ -5,6 +5,7 @@ import Lecture.AlgebraicDataTypes
 import LearnYouAHaskell.StartingOut as Starting
 import LearnYouAHaskell.TypesAndTypeclasses as Types
 import LearnYouAHaskell.SyntaxInFunctions as Syntax
+import LearnYouAHaskell.Recursion as Recursion
 
 import Homework.Homework1
 import Homework.Homework2.LogAnalysis
@@ -17,3 +18,10 @@ main = do
     print $ Syntax.describeList [1, 2, 4]
     print $ Syntax.describeList [1]
     print $ Syntax.describeList []
+    print $ Recursion.maximum' [1,2,3,4,7,19,1,2]
+    print $ Recursion.maximum2' [1,2,3,4,7,19,1,2]
+    print $ Recursion.replicate' 3 5
+    print $ Recursion.take' 4 [1,2,3,4,5,6]
+    print $ Recursion.reverse' [1,2,3,4,5]
+    print $ Recursion.zip' [1,2,3] [4,5,6]
+    print $ Recursion.elem' "r" ["a", "b", "c"]
