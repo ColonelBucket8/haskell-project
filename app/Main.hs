4297f9f13190c5e31eedf1b5eabcd95cbd5b48fc
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2022-07-08 06:10:06 +0800

    added higher order functions

--- app/Main.hs
+++ app/Main.hs
@@ -6,18 +6,13 @@ import LearnYouAHaskell.StartingOut as Starting
 import LearnYouAHaskell.TypesAndTypeclasses as Types
 import LearnYouAHaskell.SyntaxInFunctions as Syntax
 import LearnYouAHaskell.Recursion as Recursion
+import LearnYouAHaskell.HigherOrderFunctions as Higher
 
 import Homework.Homework1
 import Homework.Homework2.LogAnalysis
 
 main :: IO ()
 main = do 
-    print $ Syntax.cylinder 4 6
-    print $ Syntax.calcBmis2 [ ( 70, 160 ), (80, 150) ]
-    print $ Syntax.head2' [1,3,56,7]
-    print $ Syntax.describeList [1, 2, 4]
-    print $ Syntax.describeList [1]
-    print $ Syntax.describeList []
     print $ Recursion.maximum' [1,2,3,4,7,19,1,2]
     print $ Recursion.maximum2' [1,2,3,4,7,19,1,2]
     print $ Recursion.replicate' 3 5
@@ -26,4 +21,10 @@ main = do
     print $ Recursion.zip' [1,2,3] [4,5,6]
     print $ Recursion.elem' "r" ["a", "b", "c"]
     print $ Recursion.elem' "c" ["a", "b", "c"]
-    print $ Recursion.quicksort [5,1,3,8,4,1,45,67,0]
+    print $ Recursion.quicksort [5,1,3,8,4,1,45,67]
+    print $ Higher.applyTwice (+ 4) 20
+    print $ Higher.zipWith' (+) [1,2,3] [4,5,6]
+    print $ Higher.flip' zip [1,2] [3,4]
+    print $ map (+3) [1,2,3]
+    print $ filter (>3) [1,2,3,4,5,6]
+    print $ Higher.quicksort [1,5,2,3,87,1,7,9]
