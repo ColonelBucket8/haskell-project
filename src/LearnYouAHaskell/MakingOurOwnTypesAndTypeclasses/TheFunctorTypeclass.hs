0d4fab2f33c32a245db77b4ec53489859c914c3a
Aimi Fadhil <aimiikhwanfadhilmoh@airasia.com>
2022-07-09 21:55:42 +0800

    fixed functor class

--- src/LearnYouAHaskell/MakingOurOwnTypesAndTypeclasses/TheFunctorTypeclass.hs
+++ src/LearnYouAHaskell/MakingOurOwnTypesAndTypeclasses/TheFunctorTypeclass.hs
@@ -1,19 +1,21 @@
 module LearnYouAHaskell.MakingOurOwnTypesAndTypeclasses.TheFunctorTypeclass where
 
+import LearnYouAHaskell.MakingOurOwnTypesAndTypeclasses.RecursiveDataStructures
+
 -- class Functor f where
 --     fmap :: (a -> b) -> f a -> f b
 
 -- instance Functor [] where
 --     fmap = map
 
--- instance Function Maybe where
+-- instance Functor Maybe where
 --     fmap f (Just x) = Just (f x)
 --     fmap f Nothing = Nothing
 
--- instance Functor Tree where
---     fmap f EmptyTree = EmptyTree
---     fmap f (Node x leftsub rightsub) = Node (f x) (fmap f leftsub) (fmap f rightsub)
+instance Functor Tree where
+    fmap f EmptyTree = EmptyTree
+    fmap f (Node x leftsub rightsub) = Node (f x) (fmap f leftsub) (fmap f rightsub)
 
-instance Functor (Either a) where
-    fmap f (Right x) = Right (f x)
-    fmap f (Left x) = Left x
+-- instance Functor (Either a) where
+--     fmap f (Right x) = Right (f x)
+--     fmap f (Left x) = Left x
