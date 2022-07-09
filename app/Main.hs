7a031a022e5481309be24c9c7d64e727f0ea585b
Aimi Fadhil <aimiikhwanfadhilmoh@airasia.com>
2022-07-09 20:57:28 +0800

    added type synonyms and recursive data structures

--- app/Main.hs
+++ app/Main.hs
@@ -15,40 +15,14 @@ import LearnYouAHaskell.MakingOurOwnTypesAndTypeclasses.Shape as Shape
 import LearnYouAHaskell.MakingOurOwnTypesAndTypeclasses.RecordSyntax as Record
 import LearnYouAHaskell.MakingOurOwnTypesAndTypeclasses.TypeParameters as TypeParam
 import LearnYouAHaskell.MakingOurOwnTypesAndTypeclasses.DerivedInstances as Derived
+import LearnYouAHaskell.MakingOurOwnTypesAndTypeclasses.TypeSynonyms as TypeSynonyms
+import LearnYouAHaskell.MakingOurOwnTypesAndTypeclasses.RecursiveDataStructures as Recursive
 
 import Homework.Homework1
 import Homework.Homework2.LogAnalysis
 
 main :: IO ()
 main = do 
-    print $ Higher.applyTwice (+ 4) 20
-    print $ Higher.zipWith' (+) [1,2,3] [4,5,6]
-    print $ Higher.flip' zip [1,2] [3,4]
-    print $ map (+3) [1,2,3]
-    print $ filter (>3) [1,2,3,4,5,6]
-    print $ Higher.quicksort [1,5,2,3,87,1,7,9]
-    print $ Higher.largestDivisible 
-    print $ sum $ takeWhile (<100) $ filter odd $ map (^2) [1..]
-    print $ Higher.chain 1000
-    print $ Higher.chain2 1000
-    print $ Higher.numLongChains
-    print $ Higher.numLongChains2
-    print $ map (\x -> x + 1) [1..10]
-    print $ Higher.zipWith' (\a b -> a + b) [1,2,3,4] [5,6,7,8]
-    print $ Higher.sum' [1,2,3,4,5,6]
-    print $ Higher.sum2' [1,2,3,4,5,6]
-    print $ Higher.elem' 5 [1,2,3,4,5,6,7]
-    print $ Higher.map' (+3) [1,2,3,4,5]
-    print $ Higher.map' (*2) [1,2,3,4,5]
-    print $ Higher.map2' (*2) [1,2,3,4,5]
-    print $ Higher.maximum' [1,2,3,6,2,1]
-    print $ Higher.reverse' [1,2,3,4]
-    print $ Higher.product' [1,2,3]
-    print $ Higher.filter' (>3) [2,6,2,1,7]
-    print $ Higher.head' [4,1,2,3]
-    print $ Higher.last' [1,2,3,4,5]
-    print $ Higher.sqrtSums 
-    print $ Higher.oddSquareSum
     print $ Modules.numUniques [1,2,2,3,1,5]
     print $ Modules.search "Cat" "Cat in the jungle"
     print $ Modules.encode 3 "Hello world!"
@@ -71,4 +45,13 @@ main = do
     print $ TypeParam.multiplyVector vectorA 2
     print $ TypeParam.scalarMult vectorA vectorB
     print $ Wednesday
+    let phoneBook = [("ezzy","1234"),("anne","81824"),("bob", "191234")]
+    print $ TypeSynonyms.inPhoneBook "ezzy" "12" phoneBook
+    print $ TypeSynonyms.inPhoneBook "ezzy" "1234" phoneBook
+    print $ TypeSynonyms.lockers
+    print $ TypeSynonyms.lockerLookup 101 TypeSynonyms.lockers 
+    print $ TypeSynonyms.lockerLookup 109 TypeSynonyms.lockers 
+    print $ TypeSynonyms.lockerLookup 120 TypeSynonyms.lockers 
+    let nums = [1,2,3,4,5,6,2,1,3,4]
+    print $ foldr treeInsert EmptyTree nums
     
---
+++ src/LearnYouAHaskell/MakingOurOwnTypesAndTypeclasses/TypeSynonyms.hs
@@ -0,0 +1,36 @@
+module LearnYouAHaskell.MakingOurOwnTypesAndTypeclasses.TypeSynonyms where
+
+import qualified Data.Map as Map
+
+type Name = String
+type PhoneNumber = String
+type PhoneBook = [(Name, PhoneNumber)]
+
+inPhoneBook :: Name -> PhoneNumber -> PhoneBook -> Bool
+inPhoneBook name phoneNumber phoneBook = (name, phoneNumber) `elem` phoneBook
+
+type AssocList k v = [(k,v)]
+type IntMap v = Map.Map Int v
+
+
+data LockerState = Taken | Free deriving (Show, Eq)
+type Code = String
+type LockerMap = Map.Map Int (LockerState, Code)
+
+lockerLookup :: Int -> LockerMap -> Either String Code
+lockerLookup lockerNumber map =
+    case Map.lookup lockerNumber map of
+        Nothing -> Left $ "Locker Number " ++ show lockerNumber ++ " doesn't exist!"
+        Just (state, code) -> if state /= Taken
+                                then Right code
+                                else Left $ "Locker " ++ show lockerNumber ++ " is already taken!"
+
+lockers :: LockerMap  
+lockers = Map.fromList   
+    [(100,(Taken,"ZD39I"))  
+    ,(101,(Free,"JAH3I"))  
+    ,(103,(Free,"IQSA9"))  
+    ,(105,(Free,"QOTSA"))  
+    ,(109,(Taken,"893JJ"))  
+    ,(110,(Taken,"99292"))  
+    ]  
