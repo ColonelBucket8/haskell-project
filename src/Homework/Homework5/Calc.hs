56f8dfc0a815b08d09fbb63891b5dcfde3d7c3c2
ColonelBucket8 <freedomterran89@gmail.com>
2022-08-09 21:42:29 +0800

    added exercise 2

--- src/Homework/Homework5/Calc.hs
+++ src/Homework/Homework5/Calc.hs
@@ -1,6 +1,7 @@
 module Homework.Homework5.Calc where
 
 import Homework.Homework5.ExprT
+import Homework.Homework5.Parser
 
 -- Exercise 1
 eval :: ExprT -> Integer
@@ -8,6 +9,12 @@ eval (Lit x) = x
 eval (Add l r) = eval l + eval r
 eval (Mul l r) = eval l * eval r 
 
+-- Exercise 2
+evalStr :: String -> Maybe Integer
+evalStr str = maybe Nothing (Just . eval) (parseExp Lit Add Mul str)
+
 main :: IO ()
 main = do
     print $ eval (Mul (Add (Lit 2) (Lit 3)) (Lit 4))
+    print $ evalStr "(2+3)*4"
+
