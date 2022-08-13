2ed18ac49698196ffb6dbda8750ccd3e81e03e58
ColonelBucket8 <freedomterran89@gmail.com>
2022-08-13 12:54:26 +0800

    added exercise 2

--- src/Homework/Homework6/Fibonacci.hs
+++ src/Homework/Homework6/Fibonacci.hs
@@ -9,10 +9,20 @@ fib x = fib (x-1) + fib(x-2)
 fibs1 :: [Integer]
 fibs1 = map fib [0..]
 
+-- Exercise 2
+-- More efficient fibonacci
+fibs2Val :: Integer -> Integer -> [Integer]
+fibs2Val x y = z : fibs2Val y z
+    where z = x + y
+
+fibs2 :: [Integer]
+fibs2 = 0 : 1 : fibs2Val 0 1 
+
 
 main :: IO ()
 main = do
     print $ fib 3
     print $ fib 5
     print $ fib 7
-    print $ fibs1
+    -- print $ fibs1
+    -- print $ fibs2
