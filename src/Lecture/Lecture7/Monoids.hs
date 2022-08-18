284c4d26a49501cd2798a541d69e176c39a31281
ColonelBucket8 <freedomterran89@gmail.com>
2022-08-18 21:37:43 +0800

    added monoid maybe

--- src/Lecture/Lecture7/Monoids.hs
+++ src/Lecture/Lecture7/Monoids.hs
@@ -1,6 +1,7 @@
 module Lecture.Lecture7.Monoids where
 
 import Data.Monoid
+import qualified Data.Foldable as F
 
 -- class Monoid m where
 --     mempty :: m
@@ -58,3 +59,45 @@ lengthCompareWithVowel x y = (length x `compare` length y) `mappend`
                              (vowels x `compare` vowels y) `mappend`
                              (x `compare` y)
               where vowels = length . filter (`elem` "aeiou")
+
+-- Maybe the monoid
+-- instance Monoid a => Monoid (Maybe a) where
+--     mempty = Nothing
+--     Nothing `mappend` m = m
+--     m `mappend` Nothing = m
+--     Just m1 `mappend` Just m2 = Just (m1 `mappend` m2)
+
+newtype First a = First { getFirst :: Maybe a }
+    deriving (Eq, Ord, Read, Show)
+    
+-- instance Monoid (First a) where
+--     mempty = First Nothing
+--     First (Just x) `mappend` _ = First (Just x)
+--     First Nothing `mappend` x = x
+
+-- Using monoids to fold data structures
+data Tree a = Empty | Node a (Tree a) (Tree a) deriving (Show, Read, Eq)
+
+-- foldMap :: (Monoid M, Foldable t) => (a -> m) -> t a -> m
+
+instance F.Foldable Tree where
+    foldMap f Empty = mempty
+    foldMap f (Node x l r) = F.foldMap f l `mappend`
+                             f x           `mappend`
+                             F.foldMap f r
+
+testTree :: Tree Integer
+testTree = Node 5
+            (Node 3
+                (Node 1 Empty Empty)
+                (Node 6 Empty Empty)
+            )
+            (Node 9
+                (Node 8 Empty Empty)
+                (Node 10 Empty Empty)
+            )
+
+main :: IO ()
+main = do
+    print $ F.foldl (+) 0 testTree
+    print $ F.foldMap (\x -> [x]) testTree
