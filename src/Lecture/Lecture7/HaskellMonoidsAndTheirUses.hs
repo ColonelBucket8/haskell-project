8c019ff3e493e71098f2139b9e42636831f1ba89
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2022-08-20 17:16:55 +0800

    added some uses of monads

--- src/Lecture/Lecture7/HaskellMonoidsAndTheirUses.hs
+++ src/Lecture/Lecture7/HaskellMonoidsAndTheirUses.hs
@@ -1,2 +1,28 @@
 module Lecture.Lecture7.HaskellMonoidsAndTheirUses where
 
+-- class Monoid m where
+--     mappend :: m -> m -> m
+--     mempty :: m
+
+-- instance Monoid a where
+--     mappend = (++)
+--     mempty = []
+
+-- Rules
+-- a `mappend` mempty = a
+-- mempty `mappend` a = a
+-- (a `mmapend` b) `mappend` c == a `mappend` (b `mappend` c)
+
+-- Some Uses of Monoids
+-- instance Monoid Integer where
+--     mappend = (+)
+--     mempty = 0
+
+-- instance Monoid Integer where
+--     mappend = (*)
+--     mempty = 1
+
+-- Num a => Monoid (Sum a)
+-- Num a => Monoid (Product a)
+
+-- The Writer Monad
