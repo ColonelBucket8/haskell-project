acef59c7ac7853887b0177d516abcf3c560b0c60
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2022-08-20 17:33:00 +0800

    added the writer monads

--- src/Lecture/Lecture7/HaskellMonoidsAndTheirUses.hs
+++ src/Lecture/Lecture7/HaskellMonoidsAndTheirUses.hs
@@ -1,5 +1,10 @@
 module Lecture.Lecture7.HaskellMonoidsAndTheirUses where
 
+import Data.Monoid
+import Data.Foldable
+import Control.Monad.Writer
+import Control.Monad.State
+
 -- class Monoid m where
 --     mappend :: m -> m -> m
 --     mempty :: m
@@ -26,3 +31,50 @@ module Lecture.Lecture7.HaskellMonoidsAndTheirUses where
 -- Num a => Monoid (Product a)
 
 -- The Writer Monad
+fact1 :: Integer -> Writer String Integer
+fact1 0 = return 1
+fact1 n = do
+    let n' = n-1
+    tell $ "We've taken one away from " ++ show n ++ "\n"
+    m <- fact1 n'
+    tell $ "We've called f " ++ show m ++ "\n"
+    let r = n*m
+    tell $ "We've multiplied " ++ show n ++ " and " ++ show m ++ "\n"
+    return r
+
+fact2 :: Integer -> Writer (Sum Integer) Integer
+fact2 0 = return 1
+fact2 n = do
+    let n' = n - 1
+    tell $ Sum 1
+    m <- fact2 n' 
+    let r = n*m
+    tell $ Sum 1
+    return r
+
+fact3 :: Integer -> State Integer Integer
+fact3 0 = return 1
+fact3 n = do
+    let n' = n - 1
+    modify (+1)
+    m <- fact3 n'
+    let r = n*m
+    modify (+1)
+    return r
+
+fact4 :: Integer -> Writer Any Integer
+fact4 0 = return 1
+fact4 n = do
+    let n' = n-1
+    m <- fact4 n'
+    let r = n*m
+    tell (Any (r==120))
+    return r
+
+main :: IO ()
+main = do
+    print $ runWriter (fact1 10)
+    print $ runWriter (fact2 10)
+    print $ runState (fact3 10) 0
+    print $ runWriter (fact4 10)
+
