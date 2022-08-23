f9042179cc6ab09f2b2fae9f72037bb775559010
ColonelBucket8 <freedomterran89@gmail.com>
2022-08-23 21:46:47 +0800

    added folding expressions

--- src/Lecture/Lecture7/FoldsAndMonoids.hs
+++ src/Lecture/Lecture7/FoldsAndMonoids.hs
@@ -53,6 +53,26 @@ flatten' = treeFold [] (\l x r -> l ++ [x] ++ r)
 treeMax :: (Ord a, Bounded a) => Tree a -> a
 treeMax = treeFold minBound (\l x r -> l `max` x `max` r)
 
+-- Folding expressions
+data ExprT = Lit Integer
+           | Add ExprT ExprT
+           | Mul ExprT ExprT
+
+eval :: ExprT -> Integer
+eval (Lit i) = i
+eval (Add e1 e2) = eval e1 + eval e2
+eval (Mul e1 e2) = eval e1 * eval e2
+
+exprTFold :: (Integer -> b) -> (b -> b -> b) -> (b -> b -> b) -> ExprT -> b
+exprTFold f _ _ (Lit i) = f i
+exprTFold f g h (Add e1 e2) = g (exprTFold f g h e1) (exprTFold f g h e2)
+exprTFold f g h (Mul e1 e2) = h (exprTFold f g h e1) (exprTFold f g h e2)
+
+eval2 :: ExprT -> Integer
+eval2 = exprTFold id (+) (*)
+
+numLiterals :: ExprT -> Int
+numLiterals = exprTFold (const 1) (+) (+)
 
 main :: IO ()
 main = do
