0cba528613ba62d2e3eb11de1a6226333032fad9
ColonelBucket8 <freedomterran89@gmail.com>
2022-08-24 22:28:03 +0800

    added monoids

--- src/Lecture/Lecture7/FoldsAndMonoids.hs
+++ src/Lecture/Lecture7/FoldsAndMonoids.hs
@@ -74,6 +74,51 @@ eval2 = exprTFold id (+) (*)
 numLiterals :: ExprT -> Int
 numLiterals = exprTFold (const 1) (+) (+)
 
+-- Monoids
+-- class Monoid m where
+--    mempty :: m
+--    mappend :: m -> m -> m
+
+--    mconcat :: [m] -> m
+--    mconcat = foldr mappend mempty
+
+(<>) :: Monoid m => m -> m -> m
+(<>) = mappend
+
+-- instance Monoid [a] where
+--    mempty = []
+--    mappend = (++)
+
+-- newtype Sum a = Sum a
+--    deriving (Eq, Ord, Num, Show)
+
+-- getSum :: Sum a -> a
+-- getSum (Sum a) = a
+
+-- instance Num a => Monoid (Sum a) where
+--    mempty = Sum 0
+--    mappend = (+)
+
+-- newtype Product a = Product a
+--    deriving (Eq, Ord, Num, Show)
+
+-- getProduct :: Product a -> a
+-- getProduct (Product a) = a
+
+-- instance Num a => Monoid (Product a) where
+--    mempty = Product 1
+--    mappend = (*)
+
+lst :: [Integer]
+lst = [1,2,3,4]
+
+-- prod :: Integer
+-- prod = getProduct . mconcat . map Product $ lst
+
+-- instance (Monoid a, Monoid b) => Monoid (a,b) where
+--    mempty = (mempty, mempty)
+--    (a,b) `mappend` (c,d) = (a `mappend` c, b `mappend` d) 
+
 main :: IO ()
 main = do
     print $ treeSize' treeEx2
