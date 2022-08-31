e4527e4c77ecd09aff15340337473c01c3d5e6d3
ColonelBucket8 <freedomterran89@gmail.com>
2022-08-31 19:50:18 +0800

    added palindromes

---
+++ src/Lecture/Lecture8/Palindromes.hs
@@ -0,0 +1,10 @@
+module Lecture.Lecture8.Palindromes where
+
+main :: IO ()
+main = do
+        interact respondPalindromes
+
+respondPalindromes :: String -> String
+respondPalindromes = unlines . map (\xs -> if isPalindrome xs then "palindrome" else "not a palindrome") . lines
+        where isPalindrome xs = xs == reverse xs
+
