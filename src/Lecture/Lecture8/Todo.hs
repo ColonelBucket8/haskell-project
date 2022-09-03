7f7fbbeea6e5521a42a9ff5374e60b5e3bcf2ac3
ColonelBucket8 <freedomterran89@gmail.com>
2022-09-03 20:49:22 +0800

    added todo program

---
+++ src/Lecture/Lecture8/Todo.hs
@@ -0,0 +1,46 @@
+module Lecture.Lecture8.Todo where
+
+import System.Environment
+import System.Directory
+import System.IO
+import Data.List
+
+dispatch :: [(String, [String] -> IO ())]
+dispatch = [ ("add", add)
+           , ("view", view)
+           , ("remove", remove)
+           ]
+
+add :: [String] -> IO ()
+add [fileName, todoItem] = appendFile fileName (todoItem ++ "\n")
+
+view :: [String] -> IO ()
+view [fileName] = do
+  contents <- readFile fileName
+  let todoTasks = lines contents
+      numberedTasks = zipWith (\n line -> show n ++ "-" ++ line) [0..] todoTasks
+  putStr $ unlines numberedTasks
+
+remove :: [String] -> IO ()
+remove [fileName, numberString] = do
+  handle <- openFile fileName ReadMode
+  (tempName, tempHandle) <- openTempFile "." "temp"
+  contents <- hGetContents handle
+  let number = read numberString
+      todoTasks = lines contents
+      newTodoItems = delete (todoTasks !! number) todoTasks
+  hPutStr tempHandle $ unlines newTodoItems
+  hClose handle
+  hClose tempHandle
+  removeFile fileName
+  renameFile tempName fileName
+
+main :: IO ()
+main = do
+  (command:args) <- getArgs
+  -- lookup key assocs  
+  -- looks up a key in an association list
+  -- lookup :: Eq a => a -> [(a,b)] -> Maybe b
+  let (Just action) = lookup command dispatch
+  action args
+
